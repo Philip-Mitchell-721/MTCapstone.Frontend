09cb4550d1ef4b8cc6754197d415c20d0ed7e5d5
Philip Mitchell <122426528+Philip-Mitchell-721@users.noreply.github.com>
2023-06-26 17:24:27 -0400

    add flexible request to apiFetchAsync 6-26-23

--- js/app.js
+++ js/app.js
@@ -2,6 +2,12 @@
   "use strict";
   app.showPassword = showPassword;
 
+  app.index = async () => {};
+
+  app.signinPage = async () => {
+    wireSigninForm();
+  };
+
   app.registerPage = async () => {
     wireRegisterForm();
   };
@@ -13,6 +19,7 @@
     // button.addEventListener("click", submitRegisterForm);
     form.onsubmit = submitRegisterForm;
   }
+
   async function submitRegisterForm(e) {
     e.preventDefault();
     const form = document.querySelector("#register form");
@@ -32,39 +39,106 @@
     //   location.href = "../";
     // }
   }
-  async function apiFetchAsync(route, httpMethod, data) {
+
+  function wireSigninForm() {
+    const form = document.querySelector("#sign-in form");
+    // const button = document.getElementById("register-button");
+    // button.addEventListener("click", submitRegisterForm);
+    console.log(form);
+    form.onsubmit = submitSigninForm;
+  }
+  async function submitSigninForm(e) {
+    e.preventDefault();
+    const form = document.querySelector("#sign-in form");
+    const requestDto = {};
+    requestDto.userName = form.querySelector("#username").value;
+    requestDto.password = form.querySelector("#password").value;
+    console.log(requestDto);
+
+    // const response = await apiFetchAsync(
+    //   "Authentication/login",
+    //   "POST",
+    //   requestDto
+    // );
+    // console.log(response);
+  }
+
+  async function apiFetchAsync(route, httpMethod, data, needsAuth = true) {
     const url = "https://localhost:7277/api/" + route;
+
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("Access-Control-Allow-Headers", "*");
+    myHeaders.append("Accept", "application/json");
+
+    if (needsAuth) {
+      const tokens = getTokens();
+      myHeaders.append("Authorization", `Bearer ${tokens.accessToken}`);
+    }
+
+    const myOptions = {
+      method: httpMethod,
+      headers: myHeaders,
+      mode: "cors",
+    };
+    if (httpMethod == "POST" || httpMethod == "PUT" || httpMethod == "PATCH") {
+      myOptions.body = JSON.stringify(data);
+    }
+
+    const request = new Request(url, myOptions);
+
     try {
-      const rawResponse = await fetch(url, {
-        method: `${httpMethod}`, // *GET, POST, PUT, DELETE, etc.
-        mode: "cors", // no-cors, *cors, same-origin
-        headers: {
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Headers": "*",
-          Accept: "application/json",
-          //TODO: get accessToken
-          Authorization: `Bearer ${(accessToken = "")}`,
-        },
-        body: JSON.stringify(data), // body data type must match "Content-Type" header
-      });
       // TODO: write out piece to get accessToken from localStorage and
       // check to see if it is still valid (expiration).  Handle with refresh
       // if needed, then attach to request.
-
       // TODO: look up redirect urls with query params
       // TODO: store the response from get personal decks in sessionStorage,
       //       then when adding/removing cards add them to the cache in
       //       addition to fetching
       // rawResponse.
-
+      const rawResponse = await fetch(request);
+      if (rawResponse.StatusCode == 401) {
+        refreshTokensAsync();
+        apiFetchAsync(route, httpMethod, data, needsAuth);
+      }
       const response = await rawResponse.json();
       return response;
     } catch (error) {
       console.log(error);
     }
-    // if (response.StatusCode != 401) {
-    //   return response;
-    // }
+  }
+
+  async function refreshTokensAsync() {
+    const oldTokens = getTokens();
+    const response = await apiFetchAsync(
+      "Authentication/refresh",
+      "POST",
+      oldTokens,
+      false
+    );
+    if (response.ok) {
+      const newTokens = response.json();
+      storeTokens(newTokens.value);
+    } else {
+      goToSignIn();
+    }
+  }
+
+  function getTokens() {
+    const tokens = localStorage.getItem("tokens");
+    if (!tokens) {
+      goToSignIn();
+    }
+
+    return tokens;
+  }
+
+  function goToSignIn() {
+    location.href = "/account/signin.html";
+  }
+
+  function storeTokens(tokens) {
+    localStorage.set("tokens", tokens);
   }
 
   function showPassword() {
