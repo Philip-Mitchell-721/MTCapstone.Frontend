f11fdb60aa89b035f5e6ed7874ac76cbd09acf3b
Philip Mitchell <122426528+Philip-Mitchell-721@users.noreply.github.com>
2023-07-19 23:05:40 -0400

    Add card search and add card to deck page

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 (function (app) {
   ("use strict");
   app.showPassword = showPassword;
+  app.timerId;
+
+  //TODO: Remember to undo this part to test API again.
+  app.deck = JSON.parse(localStorage.getItem("deck")) ?? {};
   //functions that run on every page
   navBarStatus();
   setCopyrightDate();
@@ -17,7 +21,6 @@
   };
 
   app.personalPage = async () => {
-    navBarStatus();
     personalPageSetup();
   };
 
@@ -27,6 +30,10 @@
 
   app.requestEmailConfirmationPage = () => {
     requestEmailConfirmationPageSetup();
+  };
+
+  app.viewDeckPage = async () => {
+    deckPageSetup();
   };
 
   /////////////////////////////////////////////////////////////////////////////////
@@ -57,13 +64,11 @@
 
     const request = new Request(encodedURL, myOptions);
 
-    debugger;
+    // debugger;
     let rawResponse;
     try {
       rawResponse = await fetch(request);
     } catch (error) {
-      // console.log(error.name);
-      // console.log(error.message);
       return {
         errors: [`${error.message}`],
         success: false,
@@ -81,6 +86,10 @@
       //The only time this should happen is when there is no body to the response,
       //which is only true if the api returned ok(), nocontent(), etc.
       //Otherwise, the api sends back Response<T>.  In all cases, the request was successful.
+
+      // TODO: This isn't true, take some time to work through this and figure out how this SHOULD work.  There are
+      // several asp httpresponse helper methods that don't return a body, and .json() will error.  That has
+      // nothing to do with whether or not the api fetch was successful.
       return { success: true };
     }
   }
@@ -253,7 +262,7 @@
 
   function personalPageSetup() {
     // TODO: uncomment getFormatOptions
-    // getFormatOptions();
+    getFormatOptions();
     document.getElementById("add-deck-form").onsubmit = addDeckAsync;
     // document
     //   .getElementById("create-deck")
@@ -263,16 +272,163 @@
     });
 
     getPersonalDecks();
-    getDeckById(10);
+  }
+
+  async function deckPageSetup() {
+    cardSearchSetup();
+    await getDeckAsync();
+    displayDeck();
+  }
+
+  function cardSearchSetup() {
+    document
+      .querySelector("#card-search")
+      .addEventListener("input", autoCompleteCardNamesDelay);
+
+    document.querySelector(".results-list").addEventListener("click", (e) => {
+      throttleClick(addCardByScryfallId, 2000, e);
+    });
+
+    document.getElementById("add-card-form").onsubmit = formDoNothing;
   }
 
   /////////////////////////////////////////////////////////////////////////////////
+  async function addCardByScryfallId(e) {
+    e.preventDefault();
+    const cardNameToAdd = e.target.innerText;
+    const card = await getCardFromscryfallAsync(cardNameToAdd);
+    if (!card) {
+      // TODO: handle when card is not found from scryfall
+      return;
+    }
+
+    const requestDto = {};
+    requestDto.scryfallId = card.id;
+    console.log(app.deck.id);
+    const response = await apiFetchAsync(
+      `decks/${app.deck.id}/cards`,
+      "POST",
+      requestDto,
+      true
+    );
+    console.log(response);
+    //TODO: Make addCard update the page upon successful response.
+  }
+
+  async function getCardFromscryfallAsync(cardNameToAdd) {
+    const encodedCardName = encodeURIComponent(cardNameToAdd);
+    try {
+      const rawResponse = await fetch(
+        `https://api.scryfall.com/cards/named?exact=${encodedCardName}`
+      );
+      const response = await rawResponse.json();
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  function throttleClick(func, delay, e) {
+    if (app.timerId) {
+      return;
+    }
+
+    func(e);
+    app.timerId = setTimeout(function () {
+      app.timerId = undefined;
+    }, delay);
+  }
+
+  async function autoCompleteCardNames() {
+    let cardName = document.getElementById("card-search").value;
+    if (!cardName || cardName.trim().length < 3) {
+      return;
+    }
+    fetch(`https://api.scryfall.com/cards/autocomplete?q=${cardName}`)
+      .then((rawResponse) => {
+        return rawResponse.json();
+      })
+      .then((response) => {
+        const div = document.querySelector(".results-list");
+        const newDiv = document.createElement("div");
+        response.data.forEach((name) => {
+          const a = document.createElement("a");
+          a.innerText = name;
+          a.classList.add("list-group-item");
+          a.classList.add("list-group-item-action");
+          newDiv.append(a);
+        });
+
+        div.innerHTML = newDiv.innerHTML;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  function autoCompleteCardNamesDelay() {
+    clearTimeout(app.timerId);
+    app.timerId = setTimeout(async () => {
+      await autoCompleteCardNames();
+      app.timerId = undefined;
+    }, 1500);
+  }
+
+  function displayDeck() {
+    //continue: deck is in app.deck, now work on pieces to display the deck
+    if (Object.keys(app.deck).length === 0) {
+      return;
+    }
+
+    document.querySelector("h1").innerText = app.deck.name;
+    const categories = getDeckCategories();
+  }
+
+  async function getDeckCategories() {
+    const categories = [];
+    const div = document.createElement("div");
+    app.deck.cards.forEach((card, index) => {
+      //TODO: this is a test, change this to display cards.
+      setTimeout(() => {
+        console.log(card);
+        const img = document.createElement("img");
+        img.src = card.imageUris.normal;
+        img.alt = card.name;
+        div.append(img);
+      }, index * 100);
+    });
+    document.querySelector("main").append(div);
+  }
+
   async function getPersonalDecks() {
     const table = document.querySelector(".deck-table");
     const tableHead = table.querySelector("thead");
     const tableBody = table.querySelector("tbody");
 
     const response = await apiFetchAsync("Decks/Personal", "GET", {}, true);
+    // const response = {
+    //   success: true,
+    //   value: [
+    //     {
+    //       id: 1,
+    //       name: "Yidris Rocks",
+    //       format: "commander",
+    //       isPrivate: true,
+    //     },
+    //     {
+    //       id: 2,
+    //       name: "Xenagos",
+    //       format: "commander",
+    //       isPrivate: false,
+    //     },
+    //     {
+    //       id: 3,
+    //       name: "Gitrog",
+    //       format: "commander",
+    //       isPrivate: true,
+    //     },
+    //   ],
+    // };
 
     if (!response?.success) {
       const errorsDiv = errorDiv(response?.errors);
@@ -289,7 +445,9 @@
 
       const name = row.insertCell(0);
       const a = document.createElement("a");
-      a.href = `../decks/${deck.Id}`;
+      //TODO: once the response is changed back so that it comes form the api
+      // check if deck.id or deck.Id works.
+      a.href = `../decks/index.html?id=${deck.id}`;
       a.innerText = `${deck.name}`;
       a.classList.add("personal-page-links");
       name.append(a);
@@ -305,8 +463,22 @@
       }
     });
   }
-  function getDeckById(deckId) {
-    location.href = `../decks/${deckId}`;
+
+  async function getDeckAsync() {
+    const params = new URLSearchParams(window.location.search);
+    //TODO: if link that was created in getPersonalDecks was changed to use deck.Id (capital I),
+    // make sure to change the params.get to "Id".
+    const deckId = Number.parseInt(params.get("id"));
+    if (isNaN(deckId)) {
+      return;
+    }
+    const response = await apiFetchAsync(`Decks/${deckId}`, "GET", {}, false);
+    if (!response?.success) {
+      const errorsDiv = errorDiv(response?.errors);
+      document.querySelector("main").append(errorsDiv);
+      return;
+    }
+    app.deck = response?.value ?? {};
   }
 
   function getFormatOptions() {
@@ -348,7 +520,6 @@
     const format = form.querySelector("#format")?.value ?? "commander";
     const button = document.getElementById("create-deck");
     const addDeckCancelButton = document.getElementById("add-deck-cancel");
-    debugger;
 
     const requestDto = {
       name: deckName,
@@ -371,20 +542,19 @@
       return;
     }
     // TODO: When response is successfull, set location to the new deck.
-    // location.href = `../decks/${response.value.Id}`;
-    // location.reload();
+    // location.href = `../decks?id=${response.value.Id}`;
+    location.reload();
   }
 
   function navBarStatus() {
     const header = document.querySelector("header");
     const oldNav = header.querySelector("nav");
-    
+
     const accessToken = localStorage.getItem("accessToken");
     if (!accessToken) {
       return;
     }
     const payLoad = parseJwt(accessToken);
-
 
     const nav = document.createElement("nav");
 
@@ -496,7 +666,9 @@
       }
     });
   }
-
+  function formDoNothing(e) {
+    e.preventDefault();
+  }
   function setCopyrightDate() {
     document.getElementById("copyright-year").innerText =
       new Date().getFullYear();
